d11f1aaff0d29555b9576c1fa9ea83b726530631
Igor <kolesnikov271291@gmail.com>
2023-08-30 23:25:48 +0400

    second commit

---
+++ src/main/java/com/project/config/SpringConfig.java
@@ -0,0 +1,25 @@
+package com.project.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("com.project")
+@EnableWebMvc
+public class SpringConfig implements WebMvcConfigurer {
+
+    private final ApplicationContext applicationContext;
+
+    private final Environment env;
+
+    @Autowired
+    public SpringConfig(ApplicationContext applicationContext, Environment env) {
+        this.applicationContext = applicationContext;
+        this.env = env;
+    }
+}
--- src/main/java/com/project/controller/HelloController.java
+++ src/main/java/com/project/controller/HelloController.java
@@ -1,4 +1,4 @@
-package controller;
+package com.project.controller;
 
 public class HelloController {
 }
